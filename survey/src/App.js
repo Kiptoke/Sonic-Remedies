d756c401331d6eebda5c5c772be87d1bb2a81281
DESKTOP-0J68V8V\Sam <sronning@umich.edu>
2021-01-20 21:11:43 -0500

    Router Set-up

--- survey/src/App.js
+++ survey/src/App.js
@@ -1,5 +1,23 @@
+import { Fragment } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Survey from "./components/survey";
+import SurveyMap from "./components/surveyMap";
+import Home from "./components/home";
+import NotFound from "./components/notFound";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Fragment>
+      <Switch>
+        <Route path="/survey" component={Survey} />
+        <Route path="/surveymap" component={SurveyMap} />
+        <Route path="/home" component={Home} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/home" />
+        <Redirect to="/not-found" />
+      </Switch>
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ survey/src/components/survey.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Survey extends Component {
+  state = {};
+  render() {
+    return 0;
+  }
+}
+
+export default Survey;
