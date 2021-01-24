51919eee087995ce81aada1681d6af58c773cbd9
Sam Ronning <sronning@umich.edu>
2021-01-24 13:58:10 -0500

    Started MC component

--- survey/src/components/multipleChoice.jsx
+++ survey/src/components/multipleChoice.jsx
@@ -1,10 +1,44 @@
-import { Component } from "react";
+import "../css/components/multipleChoice.css";
+const MultipleChoice = ({ question, answers }) => {
+  return (
+    <div className="container-fluid p-0">
+      <h1>{question}</h1>
+      {answers.map((ans) => {
+        return (
+          <div className="row align-items-center no-gutters">
+            <div className="col-1 d-flex justify-content-center">
+              <input type="radio" name="radio" />
+            </div>
+            <div className="col">
+              <h2>{ans}</h2>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+  // <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
+  //   <label class="options">
+  //     Small Business Owner or Employee
+  //     <input type="radio" name="radio" />
+  //     <span class="checkmark" />
+  //   </label>
+  //   <label class="options">
+  //     Nonprofit Owner or Employee
+  //     <input type="radio" name="radio" />
+  //     <span class="checkmark" />
+  //   </label>
+  //   <label class="options">
+  //     Journalist or Activist
+  //     <input type="radio" name="radio" />
+  //     <span class="checkmark" />
+  //   </label>
+  //   <label class="options">
+  //     Other
+  //     <input type="radio" name="radio" />
+  //     <span class="checkmark"></span>
+  //   </label>
+  // </div>
+};
 
-class multipleChoice extends Component {
-  state = {};
-  render() {
-    return 0;
-  }
-}
-
-export default multipleChoice;
+export default MultipleChoice;
--- survey/src/components/survey.jsx
+++ survey/src/components/survey.jsx
@@ -1,9 +1,26 @@
-import { Component } from "react";
+import { Component, Fragment } from "react";
+import MultipleChoice from "./multipleChoice";
 
 class Survey extends Component {
   state = {};
   render() {
-    return 0;
+    return (
+      <Fragment>
+        <MultipleChoice
+          question="If Tom has 3 apples and gives 2 of them to Martha, how many does Martha have if she started with 2 more apples than Tom?"
+          answers={["Three.", "Four.", "Seven."]}
+        />
+        <MultipleChoice
+          question="Who let the dogs out?"
+          answers={[
+            "Roof Roof Roof",
+            "Ruuuuuffff Riff RRIIAF",
+            "Rough -CAFK BLARK",
+            "What? Stupid Question.",
+          ]}
+        />
+      </Fragment>
+    );
   }
 }
 
