02c4a01e67c8da3228d61c8c42ecde18d4188569
Sam Ronning <sronning@umich.edu>
2021-02-12 16:18:21 -0500

    FixedUI improvements

--- survey/src/components/fixedUI.jsx
+++ survey/src/components/fixedUI.jsx
@@ -1,18 +1,17 @@
 import "../css/components/fixedUI.scss";
+import "../css/components/fixedUI_DT.css";
 
 const renderCircles = (numCirc, currentCirc) => {
   let circles = [];
   for (let i = 1; i <= numCirc; i++) {
     i === currentCirc
-      ? circles.push(<div className="circle circle-filled"></div>)
-      : circles.push(<div className="circle circle-empty"></div>);
+      ? circles.push(<div key={i} className="circle circle-filled"></div>)
+      : circles.push(<div key={i} className="circle circle-empty"></div>);
   }
   return circles;
 };
 
-const FixedUI = (props) => {
-  const numCirc = 4;
-  const currentCirc = 2;
+const FixedUI = ({ numQuestions: numCirc, currentQuestion: currentCirc }) => {
   return (
     <div className="fixedUI">
       <div className="circles">{renderCircles(numCirc, currentCirc)}</div>
