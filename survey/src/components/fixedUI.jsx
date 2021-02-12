d555478af09d64dc00fbe71e1265171a55bce212
Sam Ronning <sronning@umich.edu>
2021-02-12 13:45:15 -0500

    updated fixedUI

--- survey/src/components/fixedUI.jsx
+++ survey/src/components/fixedUI.jsx
@@ -1,6 +1,23 @@
 import "../css/components/fixedUI.scss";
+
+const renderCircles = (numCirc, currentCirc) => {
+  let circles = [];
+  for (let i = 1; i <= numCirc; i++) {
+    i === currentCirc
+      ? circles.push(<div className="circle circle-filled"></div>)
+      : circles.push(<div className="circle circle-empty"></div>);
+  }
+  return circles;
+};
+
 const FixedUI = (props) => {
-  return <div className="fixedUI">o o O o o o</div>;
+  const numCirc = 4;
+  const currentCirc = 2;
+  return (
+    <div className="fixedUI">
+      <div className="circles">{renderCircles(numCirc, currentCirc)}</div>
+    </div>
+  );
 };
 
 export default FixedUI;
