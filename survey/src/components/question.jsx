4d72e04547cba311deb2f0cb3a702bc48118a7be
DESKTOP-0J68V8V\Sam <sronning@umich.edu>
2021-02-15 18:08:47 -0500

    ArrowDownChosen

--- survey/src/components/question.jsx
+++ survey/src/components/question.jsx
@@ -4,20 +4,24 @@ import { useState } from "react";
 
 function renderNext(ifChosen) {
   return ifChosen ? (
-    <div className="next-button"></div>
-  ) : (
     <div className="next-button chosen"></div>
+  ) : (
+    <div className="next-button"></div>
   );
 }
 
 function Question({ question }) {
   const { ask, responses } = question;
   const [ifChosen, setIfChosen] = useState(false);
+
+  function handleChosen() {
+    setIfChosen(true);
+  }
   //Later add logic for if the question.type is not multiple choice
   return (
     <div className="question">
       <h1>{ask}</h1>
-      <MultipleChoice responses={responses} setIfChosen={setIfChosen} />
+      <MultipleChoice responses={responses} handleChosen={handleChosen} />
       {renderNext(ifChosen)}
     </div>
   );
