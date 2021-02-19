61335b293aeb11d29503b609fdb3cf0880b07a52
Sam Ronning <sronning@umich.edu>
2021-02-19 11:03:59 -0500

    Smooth Scrolling with button

--- survey/src/components/question.jsx
+++ survey/src/components/question.jsx
@@ -3,10 +3,14 @@ import MultipleChoice from "./multipleChoice";
 import { useEffect, useState, useRef } from "react";
 import { ReactComponent as NextButtonChosen } from "../vectors/arrowDownChosen.svg";
 import { ReactComponent as NextButton } from "../vectors/arrowDown.svg";
+import { HashLink } from "react-router-hash-link";
 
-function renderNext(ifChosen) {
+function renderNext(ifChosen, pos) {
+  const nextPosId = "#question" + (pos + 1);
   return ifChosen ? (
-    <NextButtonChosen className="next-button" />
+    <HashLink smooth to={nextPosId}>
+      <NextButtonChosen className="next-button" />
+    </HashLink>
   ) : (
     <NextButton className="next-button" />
   );
@@ -21,7 +25,7 @@ function usePrevious(value) {
   return ref.current;
 }
 
-function Question({ question, handleResponse }) {
+function Question({ question, handleResponse, pos }) {
   const { ask, responses } = question;
   const [ifChosen, setIfChosen] = useState(false);
   const ifChosenPrev = usePrevious(ifChosen);
@@ -35,10 +39,10 @@ function Question({ question, handleResponse }) {
   }, [ifChosen, ifChosenPrev, handleResponse]);
   //Later add logic for if the question.type is not multiple choice
   return (
-    <div className="question">
+    <div className="question" id={"question" + pos}>
       <h1>{ask}</h1>
       <MultipleChoice responses={responses} handleChosen={handleChosen} />
-      <div className="next">{renderNext(ifChosen)}</div>
+      <div className="next">{renderNext(ifChosen, pos)}</div>
     </div>
   );
 }
