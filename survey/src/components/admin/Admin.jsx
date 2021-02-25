0ed66949959b6d6502678acb8561e06663034968
Mary Keta <maryketa@umich.edu>
2021-02-24 22:43:32 -0500

    create new questions admin

--- survey/src/components/admin/Admin.jsx
+++ survey/src/components/admin/Admin.jsx
@@ -4,6 +4,7 @@ import AdminSets from "./AdminSets";
 
 function Admin() {
   const [sets, setSets] = useState([]);
+  const [questions, setQuestions] = useState([]);
 
   //get sets
   useEffect(() => {
@@ -11,7 +12,12 @@ function Admin() {
       const serverSets = await fetchSets();
       setSets(serverSets);
     };
+    const getQuestions = async () => {
+      const serverQuestions = await fetchQuestions();
+      setQuestions(serverQuestions);
+    }
     getSets();
+    getQuestions();
   }, []);
 
   //fetch sets
@@ -21,6 +27,14 @@ function Admin() {
 
     return data;
   };
+
+  //fetch questions
+  const fetchQuestions = async () => {
+    const res = await fetch("http://localhost:5000/questions");
+    const data = await res.json();
+
+    return data;
+  }
 
   const deleteSet = async (id) => {
     await fetch(`http://localhost:5000/sets/${id}`, {
@@ -46,8 +60,43 @@ function Admin() {
   };
 
   const newQuestion = async (title, type, options) => {
-    console.log("yo angelo");
+    const question = { title: title, type: type }
+    if (options !== "") {
+      const opts_arr = parseOptions(options);
+      question.options = opts_arr;
+    }
+
+    const res = await fetch("http://localhost:5000/questions", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(question),
+    });
+    const data = await res.json();
+    setQuestions([...questions, data]);
   };
+
+  function parseOptions(options) {
+    const arr = [];
+    let start_of_opt = 0;
+    for (let i = 0; i < options.length; i++) {
+      let current = options.charAt(i);
+      if (current === ';') {
+        let sub = options.substring(start_of_opt, i);
+        let trimmed = sub.trim();
+        arr.push(trimmed);
+        start_of_opt = i + 1;
+      }
+    }
+    let sub = options.substring(start_of_opt, options.length);
+    let trimmed = sub.trim();
+    if (trimmed !== "") {
+      arr.push(trimmed);
+    }
+
+    return arr;
+  }
 
   return (
     <div>
