d1810bb3b51e8031988dd12494a99590ccdfe127
Sam Ronning <sronning@umich.edu>
2021-04-14 17:31:54 -0400

    Orientation and Complete pages

---
+++ survey/src/components/setUI/completePage.jsx
@@ -0,0 +1,10 @@
+import "../../css/components/setUI/completePage.scss";
+const CompletePage = () => {
+  return (
+    <div className="global-container complete-page">
+      <h1>You have completed the survey. Thank you!</h1>
+    </div>
+  );
+};
+
+export default CompletePage;
