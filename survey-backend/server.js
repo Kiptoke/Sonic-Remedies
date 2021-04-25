10cb34d35d0da7514d716fe28009acfa4b0dea94
DESKTOP-0J68V8V\Sam <sronning@umich.edu>
2021-04-24 23:02:28 -0400

    add credential check for ssl

--- survey-backend/server.js
+++ survey-backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const fs = require("fs");
 
 //app config
 const app = express();
@@ -12,6 +13,11 @@ app.use(express.json());
 app.use(cors());
 
 //db connection (mongoose)
+const certificate = fs.readFileSync(
+  process.env.SONICREM_NET
+    ? "/ssl/certs/sonicremedies_net_ee2f8_ed3d3_1626739199_99492aa2cd009eb466f472b351714a04.crt"
+    : null
+);
 const connection_string = process.env.SONICREM_NET
   ? "mongodb+srv://cluster0.mz4hd.mongodb.net/music-app?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
   : `mongodb+srv://${process.env.SONICREM_DB_AUTH}@cluster0.mz4hd.mongodb.net/music-app?retryWrites=true&w=majority`;
@@ -20,6 +26,7 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
+    sslCert: certificate,
   })
   .catch((error) => console.log(connection_string));
 
