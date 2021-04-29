25b2fdb70d67ebf00dde21279b708145253aef3b
DESKTOP-0J68V8V\Sam <sronning@umich.edu>
2021-04-28 20:54:24 -0400

    backend https

--- survey-backend/server.js
+++ survey-backend/server.js
@@ -1,34 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-// const cors = require("cors");
-// const helmet = require("helmet");
-// const fs = require("fs");
-// const https = require("https");
-//const fs = require("fs");
+const cors = require("cors");
+const helmet = require("helmet");
+const fs = require("fs");
+const https = require("https");
 
 //app config
 const app = express();
 const port = process.env.SURVEY_API_PORT || 5000;
-// const origin = process.env.SONICREM_NET
-//   ? `https://sonicremedies.net`
-//   : "http://localhost:3000";
+const origin = process.env.SONICREM_NET
+  ? `https://sonicremedies.net`
+  : "http://localhost:3000";
 //middlewares
 app.use(express.json());
-// app.use(cors({ origin: origin }));
-// app.use(helmet());
+app.use(cors({ origin: origin }));
+app.use(helmet());
 
 //certificatation
-// let certificate = "";
-// let privateKey = "";
-// if (process.env.SONICREM_NET) {
-//   certificate = fs.readFileSync(
-//     "../ssl/certs/sonicremedies_net_ee2f8_ed3d3_1626739199_99492aa2cd009eb466f472b351714a04.crt"
-//   );
-//   privateKey = fs.readFileSync(
-//     "../ssl/keys/ee2f8_ed3d3_15f84329f0cb1ba91733cf1284c67bde.key"
-//   );
-// }
+let certificate = "";
+let privateKey = "";
+if (process.env.SONICREM_NET) {
+  certificate = fs.readFileSync(
+    "../ssl/certs/sonicremedies_net_ee2f8_ed3d3_1626739199_99492aa2cd009eb466f472b351714a04.crt"
+  );
+  privateKey = fs.readFileSync(
+    "../ssl/keys/ee2f8_ed3d3_15f84329f0cb1ba91733cf1284c67bde.key"
+  );
+}
 
 const connection_string = `mongodb+srv://${process.env.DB_AUTH}@cluster0.mz4hd.mongodb.net/music-app?retryWrites=true&w=majority`;
 mongoose
@@ -60,19 +59,19 @@ app.use("/set-order", setOrderRouter);
 //listener (this always comes last)
 
 //Host
-// if (process.env.SONICREM_NET) {
-//   https
-//     .createServer(
-//       {
-//         key: privateKey,
-//         cert: certificate,
-//       },
-//       app
-//     )
-//     .listen(port, () => console.log(`listening on localhost: ${port}`));
-//   //Local
-// } else {
-app.listen(port, () => {
-  console.log(`listening on localhost: ${port}`);
-});
-// }
+if (process.env.SONICREM_NET) {
+  https
+    .createServer(
+      {
+        key: privateKey,
+        cert: certificate,
+      },
+      app
+    )
+    .listen(port, () => console.log(`listening on localhost: ${port}`));
+  //Local
+} else {
+  app.listen(port, () => {
+    console.log(`listening on localhost: ${port}`);
+  });
+}
